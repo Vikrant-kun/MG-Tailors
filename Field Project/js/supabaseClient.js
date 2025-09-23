6a26d15ca13b1a2376331d3644a002b1281502ae
Vikrant <vikrantvinchurkar12@gmail.com>
2025-09-23 16:49:23 +0530

    Add files via upload

--- Field Project/js/supabaseClient.js
+++ Field Project/js/supabaseClient.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'PASTE_YOUR_PROJECT_URL_HERE';
-const SUPABASE_ANON_KEY = 'PASTE_YOUR_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://usjsiofkcmogxqoqbvfh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzanNpb2ZrY21vZ3hxb3FidmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTY1NTUsImV4cCI6MjA3NDE5MjU1NX0.gvynAkhQanRlag9yp4lJ3-zQu5v8Menw9Z_inpUpLrE';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
