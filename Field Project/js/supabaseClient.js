7850d3b6d6978b92d3ce4fb49a8211a67b8c0b73
Vikrant <vikrantvinchurkar12@gmail.com>
2025-09-24 17:41:05 +0530

    Delete Field Project directory

--- Field Project/js/supabaseClient.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://usjsiofkcmogxqoqbvfh.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzanNpb2ZrY21vZ3hxb3FidmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTY1NTUsImV4cCI6MjA3NDE5MjU1NX0.gvynAkhQanRlag9yp4lJ3-zQu5v8Menw9Z_inpUpLrE';
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
